2ca947bba71eb8564dfcb7542b87e0c92b86d0a0
AgustinTardaFIUBA <atarda@fi.uba.ar>
2020-05-05 11:56:03 -0300

    Agrego ecuaciones

--- src/test.py
+++ src/test.py
@@ -1,1 +1,34 @@
-Hola heiz
+NP =  100849
+P = NP/700
+
+
+
+def calculoA( p, i,n ):
+    a = (p/i)*(((1+i)^n)-1)
+    return a
+
+
+def algo1(p, i, n, xK):
+    A = calculoA(p, i, n)
+    xKMasUno = xK - ((p/xK)*( ((1+xK)^n) -1 ) -A)
+    return xKMasUno
+
+def algo2(p, i, n, xK):
+    A = calculoA(p, i, n)
+    xKMasUno =  ((A*(xK/p)+1)^(1/n)) -1
+    return xKMasUno
+
+def algo3(p, i, n, xK):
+    A = calculoA(p, i, n)
+    xKMasUno = (p/A)*(((1+xK)^n)-1)
+    return xKMasUno
+
+def algo4(p, i, n, xK):
+    A = calculoA(p, i, n)
+    xKMasUno = xK - ((p/xK)*( ((1+xK)^n) -1 ) -A)
+    return xKMasUno
+
+def algo5(p, i, n, xK):
+    A = calculoA(p, i, n)
+    xKMasUno = xK - ((p/xK)*( ((1+xK)^n) -1 ) -A)
+    return xKMasUno
